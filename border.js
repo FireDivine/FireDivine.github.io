1855d778289bbe0bec544e843f4a36574ae3ff22
Jillian <jilli-lilli@hotmail.com>
2022-05-14 13:15:41 +1200

    made carrot clickable - changed pink border to orange on index page

---
+++ border.js
@@ -0,0 +1,8 @@
+let lola = document.getElementById('lola')
+let carrot = document.getElementById('carrot')
+
+carrot.onclick = () => {
+  // if (pic.classList.contains('OJ')) {
+  lola.classList.toggle('OJ')
+  // }
+}
